0475baf3f808684a8431ad068995f32dbdadaf3f
Arnab Ghosh <arnabsonu2@gmail.com>
2025-06-18 00:21:54 +0530

    match the colors and icons in the sidebar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import { ThemeToggle } from "@/components/theme-toggle";
+import { useSidebar } from "@/components/ui/sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard() {
+  const { open } = useSidebar();
   return (
     <div className="h-full w-full">
-      Dashboard
+      {!open && <SidebarTrigger />}
       <ThemeToggle />
     </div>
   );
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -4,12 +4,24 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
+import Logo from "./icons/logo-icon";
+import { Button } from "./ui/button";
 
 export function AppSidebar() {
   return (
     <Sidebar className="dark:border-[var(--sidebar-chatgpt-border)]">
-      <SidebarHeader />
+      <SidebarHeader className="px-2 py-0 flex justify-between flex-row header-height items-center">
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          className="[&_svg]:!w-auto [&_svg]:!h-auto hover:!bg-[var(--menu-button-highlighted)]"
+        >
+          <Logo size={24} />
+        </Button>
+        <SidebarTrigger />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
