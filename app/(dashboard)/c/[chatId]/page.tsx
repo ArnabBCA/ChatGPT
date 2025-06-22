a76d179b7ade6555652d2ca2624d72ab2df038c1
Arnab Ghosh <arnabsonu2@gmail.com>
2025-06-22 17:47:23 +0530

    added thinking animation

--- app/(dashboard)/c/[chatId]/page.tsx
+++ app/(dashboard)/c/[chatId]/page.tsx
@@ -24,6 +24,7 @@ export default function ChatId() {
   //const [isOverflowing, setIsOverflowing] = useState(false);
 
   const [loading, setLoading] = useState(false);
+  const [isThinking, setIsThinking] = useState(false);
   const [isFinishedState, setIsFinishedState] = useState(false);
 
   const userInput = useChatboxStore((state) => state.userInput);
@@ -43,6 +44,9 @@ export default function ChatId() {
     },
     onFinish: () => {
       setIsFinishedState(true);
+    },
+    onResponse: () => {
+      setIsThinking(false);
     },
   });
 
@@ -84,6 +88,7 @@ export default function ChatId() {
 
   useEffect(() => {
     if (userInput?.trim() || userFiles?.length) {
+      setIsThinking(true);
       (async () => {
         append({
           role: "user",
@@ -133,89 +138,90 @@ export default function ChatId() {
         //"px-6"
       )}
     >
-      {loading ? (
-        <span>Loading..</span>
-      ) : (
-        <div className="flex flex-col gap-10 h-full w-full max-w-[32rem] sm:max-w-[40rem] md:max-w-[48rem] mt-1.5">
-          {allMessages.map((msg, i) => (
-            <div
-              key={i}
-              className={`whitespace-pre-wrap w-full prose prose-neutral dark:prose-invert ${
-                msg.role === "user"
-                  ? "self-end px-5 pt-2.5 pb-1 dark:bg-[#323232d9] dark:text-white rounded-3xl mb-[42px] max-w-max wrap-anywhere"
-                  : "self-start"
-              }`}
-            >
-              <div className="markdown">
-                <ReactMarkdown
-                  remarkPlugins={[remarkGfm]}
-                  components={{
-                    code({ node, className, children, ...props }) {
-                      const match = /language-(\w+)/.exec(className || "");
-                      return match ? (
-                        <SyntaxHighlighter
-                          wrapLongLines={true}
-                          style={vscDarkPlus as any}
-                          language={match[1]}
-                          customStyle={{
-                            borderRadius: "0.75rem",
-                            padding: "1rem",
-                            backgroundColor: "#171717",
-                          }}
-                        >
-                          {String(children).replace(/\n$/, "")}
-                        </SyntaxHighlighter>
-                      ) : (
-                        <code
-                          className="dark:bg-[#424242] px-[0.3rem] py-[0.15rem] rounded text-sm font-mono"
-                          {...props}
-                        >
-                          {children}
-                        </code>
-                      );
-                    },
-                    a: ({ node, ...props }) => (
-                      <a
-                        {...props}
-                        className="text-blue-500 underline"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      />
-                    ),
-                  }}
-                >
-                  {typeof msg.content === "string" ? msg.content : ""}
-                </ReactMarkdown>
-                {msg.experimental_attachments &&
-                  msg.experimental_attachments.length > 0 && (
-                    <div>
-                      {msg.experimental_attachments.map((file: any) => (
-                        <div key={file.url} className="mb-1">
-                          <a
-                            href={file.url || "#"}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-blue-500 underline"
-                          >
-                            {file.name || `Attachment ${file.url + 1}`}
-                          </a>
-                        </div>
-                      ))}
-                    </div>
-                  )}
-              </div>
-              {msg.role !== "user" && <AiMessageActionButtons />}
-            </div>
-          ))}
+      <div className="flex flex-col gap-10 h-full w-full max-w-[32rem] sm:max-w-[40rem] md:max-w-[48rem] mt-1.5">
+        {allMessages.map((msg, i) => (
           <div
-            ref={scrollRef}
-            className={cn(
-              "w-full pb-[calc(100vh-300px)]",
-              messages.length > 2 ? "pb-[calc(100vh-300px)]" : "pb-25"
-            )}
-          ></div>
-        </div>
-      )}
+            key={i}
+            className={`whitespace-pre-wrap w-full prose prose-neutral dark:prose-invert ${
+              msg.role === "user"
+                ? "self-end px-5 pt-2.5 pb-1 dark:bg-[#323232d9] dark:text-white rounded-3xl mb-[42px] max-w-max wrap-anywhere"
+                : "self-start"
+            }`}
+          >
+            <div className="markdown">
+              <ReactMarkdown
+                remarkPlugins={[remarkGfm]}
+                components={{
+                  code({ node, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return match ? (
+                      <SyntaxHighlighter
+                        wrapLongLines={true}
+                        style={vscDarkPlus as any}
+                        language={match[1]}
+                        customStyle={{
+                          borderRadius: "0.75rem",
+                          padding: "1rem",
+                          backgroundColor: "#171717",
+                        }}
+                      >
+                        {String(children).replace(/\n$/, "")}
+                      </SyntaxHighlighter>
+                    ) : (
+                      <code
+                        className="dark:bg-[#424242] px-[0.3rem] py-[0.15rem] rounded text-sm font-mono"
+                        {...props}
+                      >
+                        {children}
+                      </code>
+                    );
+                  },
+                  a: ({ node, ...props }) => (
+                    <a
+                      {...props}
+                      className="text-blue-500 underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    />
+                  ),
+                }}
+              >
+                {typeof msg.content === "string" ? msg.content : ""}
+              </ReactMarkdown>
+              {msg.experimental_attachments &&
+                msg.experimental_attachments.length > 0 && (
+                  <div>
+                    {msg.experimental_attachments.map((file: any) => (
+                      <div key={file.url} className="mb-1">
+                        <a
+                          href={file.url || "#"}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-500 underline"
+                        >
+                          {file.name || `Attachment ${file.url + 1}`}
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                )}
+            </div>
+            {msg.role !== "user" && <AiMessageActionButtons />}
+          </div>
+        ))}
+        {isThinking && (
+          <div>
+            <span className="inline-block w-3 h-3 rounded-full bg-white animate-[scaleUpDown_1.2s_ease-in-out_infinite]" />
+          </div>
+        )}
+        <div
+          ref={scrollRef}
+          className={cn(
+            "w-full pb-[calc(100vh-300px)]",
+            messages.length > 2 ? "pb-[calc(100vh-300px)]" : "pb-25"
+          )}
+        ></div>
+      </div>
     </div>
   );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,27 @@
+"use client";
+
 import ChatHeader from "@/components/chat/chat-header";
 import Chatbox from "@/components/chat/chatbox";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
   return (
     <div className="flex h-screen w-full flex-col">
       <ChatHeader />
       <div className="h-full w-full flex justify-center relative">
         {children}
         <Chatbox />
-        <div className="text-xs absolute bottom-0 w-full text-center min-h-8 p-2 md:px-[60px]">
-          ChatGPT can make mistakes. Check important info. See{" "}
-          <a href="#" className="underline">
-            Cookie Preferences
-          </a>
-          .
-        </div>
+        {pathname !== "/" && (
+          <div className="text-xs absolute bottom-0 w-full text-center min-h-8 p-2 md:px-[60px]">
+            ChatGPT can make mistakes. Check important info. See{" "}
+            <a href="#" className="underline">
+              Cookie Preferences
+            </a>
+            .
+          </div>
+        )}
       </div>
     </div>
   );
