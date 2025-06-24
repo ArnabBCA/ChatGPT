054f2145aa5372bee34a08335042618b37d2d866
Arnab Ghosh <arnabsonu2@gmail.com>
2025-06-24 14:21:57 +0530

    fixed css

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,9 @@
-"use client";
-
 import ChatHeader from "@/components/chat/chat-header";
-import Chatbox from "@/components/chat/chatbox";
-import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-svh w-full flex-col">
       <ChatHeader />
       <div className="h-full w-full flex justify-center flex-col">
         {children}
